3a35e334058bd4880711e3a5791bda88446bf9b0
Parthiv Rawat <parthiv05022000@gmail.com>
2026-01-23 08:06:11 +0530

    chore: update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,12 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        global: 'writable',
+        vi: 'readonly',
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
